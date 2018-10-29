c4a17da56227dca91090fafb00242d8cf97b43b5
Ryan Chang <32244713+RyanDChang@users.noreply.github.com>
2018-10-28 17:56:41 -0700

    Combined Sean+Ryan's Contributions
    Managed to combine my work and Sean's work. Hopefully there are no residual bugs from the manual merge.

--- Assets/Scripts/Scripts_RC/CameraSystem.cs
+++ Assets/Scripts/Scripts_RC/CameraSystem.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class CameraSystem : MonoBehaviour {
 
     public GameObject trackedPlayer;
-    public float xMin = -5;
-    public float xMax = 5;
-    public float yMin = 0;
-    public float yMax = 3;
+    public float xMin = -9;
+    public float xMax = 9;
+    public float yMin = 1;
+    public float yMax = 5;
     
 	// Use this for initialization
 	void Start () {
         trackedPlayer = GameObject.FindGameObjectWithTag ("Player");
-        xMin = -5;
-        xMax = 5;
-        yMin = 0;
-        yMax = 3;
+        //xMin = -;
+        //xMax = 5;
+        //yMin = 0;
+        //yMax = 3;
 
     }
 	
